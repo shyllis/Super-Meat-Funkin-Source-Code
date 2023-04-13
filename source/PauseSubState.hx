6819a8739b590316628675d495fbbf524494b7b5
Shyllis <dolpshyosu@gmail.com>
2023-04-13 13:32:07 -0600

    fart

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -104,7 +104,7 @@ class PauseSubState extends MusicBeatSubstate {
 					if (FlxG.save.data.fpsCap > 290)
 						(cast(Lib.current.getChildAt(0), Main)).setFPSCap(290);
 					
-					transition('IN');
+					transition('INDAMNED');
 		
 					FlxG.sound.music.stop();
 		
